857d962a9ade2ad570987952625a5a60e6af8e2f
nguyen <qnguyen.1014@gmail.com>
2025-05-17 01:07:36 +0700

    added project structure (no db yet)

---
+++ SexCare.DAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SexCare.DAL.Repositories.Interfaces
+{
+    internal class IUnitOfWork
+    {
+    }
+}
