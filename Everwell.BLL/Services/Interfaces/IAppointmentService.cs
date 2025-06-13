9498661cf07cec4d476508bdcff2f727b8e78000
nguyen <qnguyen.1014@gmail.com>
2025-06-13 20:21:13 +0700

    added consultant schedule functions

--- Everwell.BLL/Services/Interfaces/IAppointmentService.cs
+++ Everwell.BLL/Services/Interfaces/IAppointmentService.cs
@@ -1,8 +1,8 @@
- using Everwell.DAL.Data.Entities;
- using Everwell.DAL.Data.Requests.Appointments;
- using Everwell.DAL.Data.Responses.Appointments;
+using Everwell.DAL.Data.Entities;
+using Everwell.DAL.Data.Requests.Appointments;
+using Everwell.DAL.Data.Responses.Appointments;
 
- namespace Everwell.BLL.Services.Interfaces;
+namespace Everwell.BLL.Services.Interfaces;
 
 public interface IAppointmentService
 {
@@ -12,4 +12,7 @@ public interface IAppointmentService
     Task<CreateAppointmentsResponse> CreateAppointmentAsync(CreateAppointmentRequest request);
     Task<CreateAppointmentsResponse> UpdateAppointmentAsync(Guid id, Appointment appointment);
     Task<DeleteAppointmentResponse> DeleteAppointmentAsync(Guid id);
+    Task<IEnumerable<GetScheduleResponse>> GetConsultantSchedules();
+    Task<IEnumerable<GetScheduleResponse>> GetConsultantSchedulesById(Guid id);
+    Task<GetScheduleResponse> CreateConsultantSchedule(CreateScheduleRequest request);
 }
